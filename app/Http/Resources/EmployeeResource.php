332edd408c5f8ed61df06c68294a33383701b870
Joelson Mutute <joelsonmutute02@gmail.com>
2025-11-15 09:20:04 +0100

    fix(employee): 🐛 corrigir EmployeeResource

--- app/Http/Resources/EmployeeResource.php
+++ app/Http/Resources/EmployeeResource.php
@@ -29,7 +29,7 @@ class EmployeeResource extends JsonResource
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
             'email' => $this->email,
-            'phone' => $this->phone_number,
+            'phone_number' => $this->phone_number,
             'image' => $finalImage,
             'has_image' => $this->image || ($this->user && $this->user->image),
             'address' => $this->address,
